caade38695ddf53e48bb5f31f682d0c7a0e01562
Varun Gupta <varun50299@gmail.com>
2025-12-25 19:43:56 +0530

    added auth middleware

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -8,6 +8,9 @@ const app=new Hono<{
   Bindings : {
     DATABASE_URL : string;
     JWT_SECRET : string;
+  },
+  Variables : {
+    userId : string
   }
 }>()
 
@@ -54,6 +57,29 @@ app.post('/api/v1/user/signin',async (c)=>{
   return c.json({
     token
   })
+})
+
+app.use('/api/v1/blog/*',async (c,next)=>{
+  const payload=c.req.header('authorization')
+  if(!payload){
+    c.status(403)
+    return c.json({
+      msg : 'Authorization failed'
+    })
+  }
+  const token=payload.split(' ')[1]
+
+  try{
+    const payload=await verify(token,c.env.JWT_SECRET)
+    //@ts-ignore
+    c.set('userId',payload.id)
+    await next()
+  }catch(e){
+    c.status(403)
+    return c.json({
+      msg : 'Authorization failed'
+    })
+  }
 })
 
 app.post('/api/v1/blog',(c)=>{
