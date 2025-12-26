2075215c940dff4f6f309aecd999cd7b603cb044
Varun Gupta <varun50299@gmail.com>
2025-12-27 00:22:52 +0530

    resolved an input validation bug

--- common/src/index.ts
+++ common/src/index.ts
@@ -15,14 +15,12 @@ export type SigninInput=zod.infer<typeof signinInput>
 
 export const createPostInput=zod.object({
     title : zod.string().min(1),
-    content : zod.string().min(1),
-    authorId : zod.string()
+    content : zod.string().min(1)
 })
 export type CreatePostInput=zod.infer<typeof createPostInput>
 
 export const updatePostInput=zod.object({
     title : zod.string().min(1),
-    content : zod.string().min(1),
-    authorId : zod.string()
+    content : zod.string().min(1)
 })
 export type UpdatePostInput=zod.infer<typeof updatePostInput>
